d7bea124f513f7e6e14eb28873be242e3cea0177
Yansh101 <priyanshu1.joshi@gmail.com>
2024-07-06 11:55:19 +0545

    hamburger icon option

--- src/component/navbar/Navbar.js
+++ src/component/navbar/Navbar.js
@@ -1,29 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const navLinks = [
+    { href: "#about", text: "About" },
+    { href: "#project", text: "Project" },
+    { href: "#contact", text: "Contact" },
+  ];
+
   return (
     <nav className="container">
       <div className="logo">Priyanshu Joshi</div>
-      <div>
-        <ul className="nav-links">
-          <li>
-            <a href="#about" className="links">
-              About
+      <ul className={`desktop-nav ${isMenuOpen ? "open" : ""}`}>
+        {navLinks.map((link) => (
+          <li key={link.href}>
+            <a href={link.href} className="links">
+              {link.text}
             </a>
           </li>
-          <li>
-            <a href="#project" className="links">
-              Project
-            </a>
-          </li>
-          <li>
-            <a href="#contact" className="links">
-              Contact
-            </a>
-          </li>
-        </ul>
+        ))}
+      </ul>
+      <div className="hamburger-icon" onClick={toggleMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
+      {isMenuOpen && (
+        <ul className="mobile-nav">
+          {navLinks.map((link) => (
+            <li key={link.href}>
+              <a href={link.href} onClick={toggleMenu}>
+                {link.text}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </nav>
   );
 }
