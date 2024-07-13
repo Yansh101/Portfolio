0b19cde81bb6b45646c3b4c5f115699e2c9c4f08
Yansh101 <priyanshu1.joshi@gmail.com>
2024-07-13 19:50:19 +0545

    CV update

--- src/component/Landing pages/Profile/Profile.js
+++ src/component/Landing pages/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React from "react";
-import profileImage from '../Photos/Profile.jpg';  // Adjust path accordingly
-import './Profile.css';
+import profileImage from "../Photos/Profile.jpg"; // Adjust path accordingly
+import "./Profile.css";
+import PriyanshuCV from "./Priyanshu-Joshi CV.pdf";
 
 function Profile() {
   return (
@@ -13,15 +14,27 @@ function Profile() {
         <h1 className="title">Priyanshu Joshi</h1>
         <p className="section__text__p2 sectionp2">Full Stack Developer</p>
         <div className="btn-container">
-          <a href="/Priyanshu-Joshi CV.pdf" download className="btn btn-color-2 btncv">Download CV</a>
-          <a href="#contact" className="btn btn-color-1 btncv">Contact Info</a>
+          <a href={PriyanshuCV} download className="btn btn-color-2 btncv">
+            Download CV
+          </a>
+          <a href="#contact" className="btn btn-color-1 btncv">
+            Contact Info
+          </a>
         </div>
         <div className="socialcontainer" id="socials-container">
-          <a href="https://www.linkedin.com/in/priyanshu-joshi-65b4371b8/" target="_blank" rel="noopener noreferrer">
+          <a
+            href="https://www.linkedin.com/in/priyanshu-joshi-65b4371b8/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src="images/linkedin.png" alt="LinkedIn" className="icon" />
           </a>
-          <a href="https://github.com/Yansh101" target="_blank" rel="noopener noreferrer">
-            <img src="images/github.png" alt="Github" className="icon" /> 
+          <a
+            href="https://github.com/Yansh101"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src="images/github.png" alt="Github" className="icon" />
           </a>
         </div>
       </div>
